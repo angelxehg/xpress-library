1ff519cdf96ba58fd8c00fc5465832d2ba67ab5e
Angel Hurtado <contact@angelxehg.com>
2020-12-03 20:05:59 -0600

    Add PWA support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('Hay una nueva versión disponible. ¿Cargar nueva versión?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
